3384d08c7765658f126f12ddb8396f7c4797bcc4
fniyonshuti <f.niyonshut1@alustudent.com>
2025-04-03 18:56:23 +0200

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,15 +8,19 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
-
   ],
   server: {
     port: process.env.PORT || 10000, // Use Render's port or default to 10000
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ["career-connect-frontend-k9z7.onrender.com"] // Allow your deployed host
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+    allowedHosts: ["career-connect-frontend-k9z7.onrender.com"], // Allow during preview mode as well
   },
   resolve: {
     alias: {
-      // '@': '/src', 
       "@": path.resolve(__dirname, "src") // Ensure '@' is mapped to your src directory
     },
   },
